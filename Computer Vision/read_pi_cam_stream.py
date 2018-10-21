448071a1e596760e71b0f4299a446530cb3e925b
Sohaib Arif <arif_sohaib@outlook.com>
2018-10-21 01:10:19 -0400

    added detection and streaming code

---
+++ Computer Vision/read_pi_cam_stream.py
@@ -0,0 +1,21 @@
+import cv2
+
+cap = cv2.VideoCapture('http://192.168.2.42:8160')
+if not cap.isOpened():
+    print('VideoCapture not opened')
+    exit(-1)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print('frame empty')
+        break
+
+    cv2.imshow('image', frame)
+
+    if cv2.waitKey(1)&0XFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
